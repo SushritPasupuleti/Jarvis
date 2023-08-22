8ad5f571b1f421d0333a7427c37a15cce58589e5
Sushrit_Lawliet <sushrit.pk21@gmail.com>
2023-08-22 13:21:31 +0530

    feat: switch to huggingface pipeline

--- model/main.py
+++ model/main.py
@@ -2,6 +2,8 @@ import torch
 from transformers import LlamaTokenizer, LlamaForCausalLM, pipeline
 from langchain import PromptTemplate, LLMChain
 from langchain.llms import HuggingFacePipeline
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
 if torch.cuda.is_available():
     device = torch.device("cuda")
@@ -10,8 +12,8 @@ if torch.cuda.is_available():
 else:
     device = torch.device("cpu")
 
-# model_path = 'openlm-research/open_llama_3b'
-model_path = 'openlm-research/open_llama_7b'
+model_path = 'openlm-research/open_llama_3b'
+# model_path = 'openlm-research/open_llama_7b'
 
 tokenizer = LlamaTokenizer.from_pretrained(model_path)
 model = LlamaForCausalLM.from_pretrained(
@@ -19,34 +21,38 @@ model = LlamaForCausalLM.from_pretrained(
 )
 
 pipe = pipeline(
-    "text-generation", model=model, tokenizer=tokenizer, max_new_tokens=10
+    "text-generation", 
+    model=model, # pyright: ignore
+    tokenizer=tokenizer, 
+    max_new_tokens=2**8,
+    device=device,
 )
 hf = HuggingFacePipeline(pipeline=pipe)
 
-# prompt = """
-# Question: What is the largest animal?
-# """
+callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
+
+template = """Question: {question}
+
+Answer: 
+"""
+
+question = "Explain recursion to me. With a code example in Python."
+
+prompt = PromptTemplate(template=template, input_variables=['question'])
 
 # hf(prompt)
 
-# llm_chain = LLMChain(prompt=prompt, llm=hf)
-
-# question = "What is the smallest animal?"
-
-# llm_chain.run(question)
-
-# print("Asking HF", hf('Q: What is the largest animal?\nA:'))
-
-prompt = 'Q: What is the largest animal?\nA:'
-input_ids = tokenizer(prompt, return_tensors="pt").input_ids
-
-if device.type == 'cuda':
-    # model.half()
-    input_ids = input_ids.to('cuda')
-
-generation_output = model.generate(# pyright: ignore
-    input_ids=input_ids, 
-    # max_new_tokens=32
-    max_new_tokens=32*2
+llm_chain = LLMChain(
+    prompt=prompt, 
+    llm=hf,
+    callback_manager=callback_manager,
+    verbose=True,
+    return_final_only=True,
 )
-print(tokenizer.decode(generation_output[0]))
+
+answer = llm_chain.run(question)
+# answer = llm_chain(question)
+# answer = llm_chain.predict(question=question)
+
+print(answer)
+
