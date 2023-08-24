085f3e96aee1912271c851704a193f8e63754143
Sushrit_Lawliet <sushrit.pk21@gmail.com>
2023-08-24 10:20:03 +0530

    feat: add api endpoint for web_answers

--- model/server.py
+++ model/server.py
@@ -2,6 +2,8 @@ from typing import Union
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
 from main import llm_chain
+from pydantic import BaseModel
+from web_answers import get_web_answers
 
 app = FastAPI()
 
@@ -22,4 +24,18 @@ async def chat(question: str):
     return {
         "question": question,
         "answer": answer
+    }
+
+class WebLinks(BaseModel):
+    links: list
+    question: str
+
+@app.post("/web/chat/")
+async def web_chat(web_links: WebLinks):
+    result = get_web_answers(web_links.links, web_links.question)
+
+    return {
+        "question": web_links.question,
+        "answer": result['answer'],
+        "source": result['source_documents']
     }
--- model/web_answers.py
+++ model/web_answers.py
@@ -3,57 +3,47 @@ from main import hf
 import torch
 import sys
 
-web_links = [
-    "https://help.braggi-ems.com/", 
-    "https://help.braggi-ems.com/leader_home", 
-    "https://help.braggi-ems.com/modules",
-    "https://help.braggi-ems.com/modules/voter_marking",
-    "https://help.braggi-ems.com/modules/voter_identification",
-    "https://help.braggi-ems.com/modules/house_number_marking",
-    "https://help.braggi-ems.com/modules/house_number_marking_plus",
-    "https://help.braggi-ems.com/modules/leader_management",
-    "https://help.braggi-ems.com/modules/reports",
-    "https://help.braggi-ems.com/modules/polling_day",
-    # "https://help.braggi-ems.com/privacy_policy",
-    # "https://help.braggi-ems.com/terms_of_service",
-] 
+def get_web_answers(web_links, question):
+    loader = WebBaseLoader(web_links)
+    documents = loader.load()
 
-loader = WebBaseLoader(web_links)
-documents = loader.load()
+    from langchain.text_splitter import RecursiveCharacterTextSplitter
 
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20)
+    all_splits = text_splitter.split_documents(documents)
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20)
-all_splits = text_splitter.split_documents(documents)
+    from langchain.embeddings import HuggingFaceEmbeddings
+    from langchain.vectorstores import FAISS
 
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.vectorstores import FAISS
+    device = "cuda" if torch.cuda.is_available() else "cpu"
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
+    model_name = "sentence-transformers/all-mpnet-base-v2"
+    model_kwargs = {"device": device}
 
-model_name = "sentence-transformers/all-mpnet-base-v2"
-model_kwargs = {"device": device}
+    embeddings = HuggingFaceEmbeddings(model_name=model_name, model_kwargs=model_kwargs)
 
-embeddings = HuggingFaceEmbeddings(model_name=model_name, model_kwargs=model_kwargs)
+    # storing embeddings in the vector store
+    vectorstore = FAISS.from_documents(all_splits, embeddings)
 
-# storing embeddings in the vector store
-vectorstore = FAISS.from_documents(all_splits, embeddings)
+    from langchain.chains import ConversationalRetrievalChain
 
-from langchain.chains import ConversationalRetrievalChain
+    chain = ConversationalRetrievalChain.from_llm(hf, vectorstore.as_retriever(), return_source_documents=True)
 
-chain = ConversationalRetrievalChain.from_llm(hf, vectorstore.as_retriever(), return_source_documents=True)
+    chat_history = []
 
-chat_history = []
+    if sys.argv[1] != None:
+        query = sys.argv[1]
+    elif question != None:
+        query = question
+    else:
+        query = "Tell me about the modules in Braggi EMS."
 
-if sys.argv[1] != None:
-    query = sys.argv[1]
-else:
-    query = "Tell me about the modules in Braggi EMS."
+    # print("Question: ", question)
 
-# print("Question: ", question)
+    result = chain({"question": query, "chat_history": chat_history})
 
-result = chain({"question": query, "chat_history": chat_history})
-
-print("Answer: ", result['answer'])
-print("\n\n ============================ \n\n")
-print("Source: ", result['source_documents'])
+    # print("Answer: ", result['answer'])
+    # print("\n\n ============================ \n\n")
+    # print("Source: ", result['source_documents'])
+    
+    return result
