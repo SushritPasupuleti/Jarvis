20663e27f2ab3e2118a130fffdd64d1d695d8bba
Sushrit_Lawliet <sushrit.pk21@gmail.com>
2023-08-24 11:52:53 +0530

    feat: add request caching

--- model/server.py
+++ model/server.py
@@ -1,41 +1,62 @@
 from typing import Union
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
-from main import llm_chain
 from pydantic import BaseModel
+from starlette.requests import Request
+from starlette.responses import JSONResponse, Response
+
+from main import llm_chain
 from web_answers import get_web_answers
+
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.inmemory import InMemoryBackend
+from fastapi_cache.decorator import cache
 
 app = FastAPI()
 
+@app.on_event("startup")
+async def startup():
+    FastAPICache.init(InMemoryBackend())
+
+cache_duration = 60 * 60 # 1 hour
+
 @app.get("/")
-def read_root():
-    return {
+@cache(namespace="jarvis", expire=cache_duration)
+async def read_root():
+    return JSONResponse({
         "message": "Hello There",
-        "status": "OK"
-    }
+        "status": "OK",
+    })
+
+@app.get("/clear-cache")
+async def clear():
+    return await FastAPICache.clear(namespace="jarvis")
 
 @app.get("/chat/{question}")
+@cache(namespace="jarvis", expire=cache_duration)
 async def chat(question: str):
     answer = llm_chain.run(question)
 
-    print("Question: ", question)
-    print("Answer: ", answer)
+    # print("Question: ", question)
+    # print("Answer: ", answer)
 
-    return {
+    return JSONResponse({
         "question": question,
-        "answer": answer
-    }
+        "answer": answer,
+    })
 
 class WebLinks(BaseModel):
     links: list
     question: str
 
 @app.post("/web/chat/")
+@cache(namespace="jarvis", expire=cache_duration)
 async def web_chat(web_links: WebLinks):
     result = get_web_answers(web_links.links, web_links.question)
 
-    return {
+    return JSONResponse({
         "question": web_links.question,
         "answer": result['answer'],
-        "source": result['source_documents']
-    }
+        "source": result['source_documents'],
+    })
+
